c6a21f75bc66446cde519a4855aa7f9596c90efa
momo r <moriki@gmail.com>
2022-10-10 22:59:03 +0300

    Impl error for shoobyError
    Signed-off-by: momo r <moriki@gmail.com>

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
 #[derive(Debug, Clone)]
 pub enum ShoobyError {
     Unknown,
@@ -6,3 +8,17 @@ pub enum ShoobyError {
     InvalidSize,
     InvalidType,
 }
+
+impl Display for ShoobyError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            ShoobyError::Unknown => write!(f, "Unknown error"),
+            ShoobyError::OutOfBounds => write!(f, "Out of bounds"),
+            ShoobyError::InvalidTypeConversion => write!(f, "Invalid type conversion"),
+            ShoobyError::InvalidSize => write!(f, "Invalid size"),
+            ShoobyError::InvalidType => write!(f, "Invalid type"),
+        }
+    }
+}
+
+impl std::error::Error for ShoobyError {}
